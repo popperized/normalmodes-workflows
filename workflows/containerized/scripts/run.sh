33e67925a570a12b65260c205d09a8cc4d141ffe
arshul <arshulmohd123@gmail.com>
2019-07-24 00:47:54 +0530

    required changes and readme added

--- workflows/containerized/scripts/run.sh
+++ workflows/containerized/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -e
 #SBATCH -J s1p1test
 #SBATCH -o s1p1test_%j.txt
 #SBATCH -e errs1p1test_%j.err
@@ -14,7 +14,6 @@
 export OMP_NUM_THREADS=2
 #export MV2_ENABLE_AFFINITY=0
 
-cd ./submodules/NormalModes/
-source SetEnv
-cd demos/
-mpirun --allow-run-as-root -np 1 ./../bin/plmvcg_stampede2.out
+pushd ./submodules/NormalModes/demos
+mpirun --allow-run-as-root -np 1 --mca btl_base_warn_component_unused 0 ./../bin/plmvcg_popper.out
+popd
